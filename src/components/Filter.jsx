6324c21e97dc56de6c5047a90d7464abcf388eff
Guillaume TILLARD <guillaumetillard10@gmail.com>
2024-07-30 19:58:08 +0200

    try on filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,49 +2,19 @@ import React, { useState } from 'react';
 
 const Filter = () => {
     const [selectedInput, setSelectedInput] = useState("");
+    const select = ["Athlétisme", "Aviron", "Badminton", "Basket-ball", "Boxe", "Canoë-kayak", "Cyclisme", "Équitation", "Escrime", "Football", "Golf", "Gymnastique", "Haltérophilie", "Handball", "Hockey sur gazon", "Judo", "Karaté", "Lutte", "Natation", "Pentathlon moderne", "Rugby à sept", "Skateboard", "Surf", "Taekwondo", "Tennis", "Tennis de table", "Tir", "Tir à l'arc", "Triathlon", "Voile", "Volleyball"];
+
 
     return (
         <section className='filter_section'>
-           <select id="sport" >
-                    <option value="" disabled selected>Sélectionner une discipline</option>
-                    <option value="Athlétisme">Athlétisme</option>
-                    <option value="Aviron">Aviron</option>
-                    <option value="Badminton">Badminton</option>
-                    <option value="Basket-ball">Basket-ball</option>
-                    <option value="Boxe">Boxe</option>
-                    <option value="Canoë-kayak">Canoë-kayak</option>
-                    <option value="Cyclisme">Cyclisme</option>
-                    <option value="Équitation">Équitation</option>
-                    <option value="Escrime">Escrime</option>
-                    <option value="Football">Football</option>
-                    <option value="Golf">Golf</option>
-                    <option value="Gymnastique">Gymnastique</option>
-                    <option value="Haltérophilie">Haltérophilie</option>
-                    <option value="Handball">Handball</option>
-                    <option value="Hockey sur gazon">Hockey sur gazon</option>
-                    <option value="Judo">Judo</option>
-                    <option value="Karaté">Karaté</option>
-                    <option value="Lutte">Lutte</option>
-                    <option value="Natation">Natation</option>
-                    <option value="Pentathlon moderne">Pentathlon moderne</option>
-                    <option value="Rugby à sept">Rugby à sept</option>
-                    <option value="Skateboard">Skateboard</option>
-                    <option value="Surf">Surf</option>
-                    <option value="Taekwondo">Taekwondo</option>
-                    <option value="Tennis">Tennis</option>
-                    <option value="Tennis de table">Tennis de table</option>
-                    <option value="Tir">Tir</option>
-                    <option value="Tir à l'arc">Tir à l'arc</option>
-                    <option value="Triathlon">Triathlon</option>
-                    <option value="Voile">Voile</option>
-                    <option value="Volleyball">Volleyball</option>
-                </select> 
-                <select name="" id="genre" >
-                    <option value="" disabled selected>Genre</option>
-                    <option value="homme">Homme</option>
-                    <option value="femme">Femme</option>
-                </select>
+            <select id="sports" onChange={(e) => console.log(e.target.value)}>
+                {select.map((discipline) => (
+                    <option key={discipline}  value={discipline === selectedInput}>{discipline}</option>
+                ))}
+            </select>
+            
         </section>
+        
     );
 };
 
