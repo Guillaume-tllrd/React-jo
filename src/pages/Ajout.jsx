ecf741fab1c465f88bc6588fa9e7440ea6d9e29c
Guillaume-tllrd <guillaumetillard10@gmail.com>
2024-07-30 12:01:18 +0200

    create and read

--- src/pages/Ajout.jsx
+++ src/pages/Ajout.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import axios from 'axios';
 
 const Ajout = () => {
+    const [name, setName] = useState("");
+    const [sport, setSport] = useState("");
+    const [goldenMedal, setGoldenMedal] = useState("");
+    const [silverMedal, setSilverMedal] = useState("");
+    const [bronzeMedal, setBronzeMedal] = useState("");
+    const [image, setImage] = useState("");
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        axios.post("http://localhost:3005/athletes", {
+            name,
+            sport,
+            goldenMedal,
+            silverMedal,
+            bronzeMedal,
+            image,   
+        });
+        setName("");
+        setSport("");
+        setGoldenMedal("");
+        setSilverMedal("");
+        setBronzeMedal("");
+        setImage("");
+    }
     return (
         <div>
            <Header /> 
            <h1>Ajouter un nouveau médaillé:</h1>
-           <form >
+           <form onSubmit={handleFormSubmit}>
                 <label htmlFor="name">Nom</label>
-                <input id='name' type="text" />
+                <input id='name' type="text" onChange={(e) => setName(e.target.value)} value={name}/>
                 
                 <label htmlFor="sport">Discipline</label>
-                <input id='sport' type="text" />
+                <input id='sport' type="text" onChange={(e) => setSport(e.target.value)} value={sport} />
 
                 <label htmlFor="golden medal">Médaille d'or</label>
-                <input id='golden medal' type="number" min="0"/>
+                <input id='golden medal' type="number" min="0" onChange={(e) => setGoldenMedal(e.target.value)} value={goldenMedal}/>
 
                 <label htmlFor="silver medal">Médaille d'argent</label>
-                <input id='silver medal' type="number" min="0"/>
+                <input id='silver medal' type="number" min="0" onChange={(e) => setSilverMedal(e.target.value)} value={silverMedal}/>
                 
                 <label htmlFor="bronze medal">Médaille de bronze</label>
-                <input id='bronze medal' type="number" min="0"/>
+                <input id='bronze medal' type="number" min="0" onChange={(e) => setBronzeMedal(e.target.value)} value={bronzeMedal}/>
 
                 <label htmlFor="image">Image</label>
-                <input id='image' type="file"/>
+                <input id='image' type="file" onChange={(e) => setImage(e.target.value)} value={image}/>
 
                 <input type="submit" value="Ajouter"/>
            </form>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import axios from 'axios';
 
 const Home = () => {
+  const [dataAthlete, setDataAthlete] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3005/athletes")
+      .then((res) => setDataAthlete(res.data)) 
+      .catch((err) => console.error(err)); 
+  }, []);
+
     return (
         <div>
             <Header/>
+            <div>
+                {dataAthlete.map((athlete) => (
+          <ul key={athlete.id}>
+            {athlete.image} 
+          </ul>
+        ))}</div>
         </div>
     );
 };
